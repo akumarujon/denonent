1556b8788bf221c5a06eb04a0e18c817943bc8a4
UwUssimo Robinson <uwussimo@icloud.com>
2022-02-02 10:12:34 +0500

    Update bot.ts

--- bot.ts
+++ bot.ts
@@ -1,6 +1,11 @@
 import { Bot } from "./deps.ts";
 
-const token: string = Deno.env.get("TOKEN") || "";
+const token = Deno.env.get("TOKEN")";
+
+if (typeof token === "undefined") {
+    throw new Error("No token found!")
+}
+
 const bot = new Bot(token);
 
 bot.on("message", (ctx) => ctx.reply("Hi there!"));
