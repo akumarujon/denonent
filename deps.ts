64f59ee877a015a8df348b227d7bd22d2218f389
bPoliar <bpoliar01@gmail.com>
2023-02-12 09:08:50 +0500

    dotenv

--- deps.ts
+++ deps.ts
@@ -3,5 +3,5 @@ export {
   Composer,
   webhookCallback,
 } from "https://deno.land/x/grammy@v1.7.0/mod.ts";
-import "https://deno.land/x/dotenv/load.ts";
-export { serve } from "https://deno.land/std/http/server.ts";
+export { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import "https://deno.land/std@0.177.0/dotenv/load.ts";
